87f2c3c4a3af44bee09a78633b9fe6b98497dc44
Kiam Mota <133913407+KiamMota@users.noreply.github.com>
2026-08-15 12:23:20 -0300

    Remove trailing slash from API_BASE_URL

--- client/env.js
+++ client/env.js
@@ -1,2 +1,2 @@
-export const API_BASE_URL = 'https://injured-marshall-happening-angeles.trycloudflare.com/';
+export const API_BASE_URL = 'https://injured-marshall-happening-angeles.trycloudflare.com';
 export const TOKEN_STORAGE_KEY = 'gv_jwt_token';
